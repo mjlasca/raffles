62ba15becb6f98900c4c5b251bc922eabb3bb202
Mario Lasluisa <mjlasca@gmail.com>
2024-05-01 22:56:11 -0500

    fix download xlsx assignments

--- app/Exports/AssigmentsExport.php
+++ app/Exports/AssigmentsExport.php
@@ -2,16 +2,37 @@
 
 namespace App\Exports;
 
-use App\Models\Assigment;
+use App\Models\Assignment;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class AssigmentsExport implements FromCollection
+class AssigmentsExport implements FromCollection, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Assigment::all();
+        return Assignment::select(
+            DB::raw('(SELECT name FROM raffles WHERE raffles.id = assignments.raffle_id) as raffle_name'),
+            'tickets_numbers',
+            DB::raw('(SELECT CONCAT(name," ",lastname) FROM users WHERE users.id = assignments.user_id) as user'),
+            'commission',
+            'updated_at',
+            DB::raw('(SELECT CONCAT(name," ",lastname) FROM users WHERE users.id = assignments.edit_user) as user_edit'),
+        )->get();
+    }
+
+    public function headings(): array
+    {
+        return [
+            'Rifa',
+            'Tickets asignados',    
+            'Usuario asignado',
+            'Comisión asignada',
+            'Fecha asinación',
+            'Asignado por'
+        ];
     }
 }
