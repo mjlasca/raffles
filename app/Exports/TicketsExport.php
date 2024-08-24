1b0d8f92cb501bef511b444775a7e425dc266a75
Mario Lasluisa <mjlasca@gmail.com>
2024-08-24 08:18:12 -0500

    estado de cuenta

--- app/Exports/TicketsExport.php
+++ app/Exports/TicketsExport.php
@@ -2,12 +2,16 @@
 
 namespace App\Exports;
 
+use App\Models\Raffle;
 use App\Models\Ticket;
+use App\Models\User;
 use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\BeforeSheet;
 
-class TicketsExport implements FromCollection, WithHeadings
+class TicketsExport implements FromCollection, WithHeadings, WithEvents
 {
     private $raffleId;
     private $userId;
@@ -58,5 +62,76 @@ class TicketsExport implements FromCollection, WithHeadings
             'Teléfono cliente',
             'Movimientos del ticket'
         ];
+    }
+
+    public function registerEvents(): array
+    {
+        if(!empty($this->userId) && !empty($this->raffleId)){
+            return [
+                BeforeSheet::class => function(BeforeSheet $event) {
+                    $sumPrice = Ticket::where('raffle_id',$this->raffleId)->where('user_id',$this->userId)->sum('price');
+                    $sumPayment = Ticket::where('raffle_id',$this->raffleId)->where('user_id',$this->userId)->sum('payment');
+                    $countTicket = Ticket::where('raffle_id',$this->raffleId)->where('user_id',$this->userId)->count();
+                    $raffle = Raffle::find($this->raffleId);
+                    $user = User::find($this->userId);
+                    $sheet = $event->sheet->getDelegate();
+                    $sheet->setCellValue('A1', 'ESTADO DE CUENTA');
+                    $sheet->setCellValue('A2', 'Fecha generación: ' . now()->format('Y-m-d'));
+                    $sheet->setCellValue('F1', 'Total rifa');
+                    $sheet->setCellValue('G1', $sumPrice);
+                    $sheet->setCellValue('F2', 'Abonado total');
+                    $sheet->setCellValue('G2', $sumPayment);
+                    $sheet->setCellValue('F3', 'Saldo pendiente');
+                    $sheet->setCellValue('G3', ($sumPrice - $sumPayment));
+                    $sheet->setCellValue('A3', 'Rifa:  ' . $raffle->name);
+                    $sheet->setCellValue('C3', 'No. Boletas:  ' . $countTicket);
+                    $sheet->setCellValue('A4', 'Vendedor(a): ' . $user->name." ".$user->lastname);
+                },
+            ];
+        }elseif(!empty($this->raffleId)){
+            return [
+                BeforeSheet::class => function(BeforeSheet $event) {
+                    $sumPrice = Ticket::where('raffle_id',$this->raffleId)->sum('price');
+                    $sumPayment = Ticket::where('raffle_id',$this->raffleId)->sum('payment');
+                    $countTicket = Ticket::where('raffle_id',$this->raffleId)->count();
+                    $raffle = Raffle::find($this->raffleId);
+                    $sheet = $event->sheet->getDelegate();
+                    $sheet->setCellValue('A1', 'ESTADO DE CUENTA');
+                    $sheet->setCellValue('A2', 'Fecha generación: ' . now()->format('Y-m-d'));
+                    $sheet->setCellValue('F1', 'Total rifa');
+                    $sheet->setCellValue('G1', $sumPrice);
+                    $sheet->setCellValue('F2', 'Abonado total');
+                    $sheet->setCellValue('G2', $sumPayment);
+                    $sheet->setCellValue('F3', 'Saldo pendiente');
+                    $sheet->setCellValue('G3', ($sumPrice - $sumPayment));
+                    $sheet->setCellValue('A3', 'Rifa:  ' . $raffle->name);
+                    $sheet->setCellValue('C3', 'No. Boletas:  ' . $countTicket);
+                },
+            ];
+        }elseif(!empty($this->userId)){
+            return [
+                BeforeSheet::class => function(BeforeSheet $event) {
+                    $sumPrice = Ticket::where('user_id',$this->userId)->sum('price');
+                    $sumPayment = Ticket::where('user_id',$this->userId)->sum('payment');
+                    $countTicket = Ticket::where('user_id',$this->userId)->count();
+                    $user = User::find($this->userId);
+                    $sheet = $event->sheet->getDelegate();
+                    $sheet->setCellValue('A1', 'ESTADO DE CUENTA');
+                    $sheet->setCellValue('A2', 'Fecha generación: ' . now()->format('Y-m-d'));
+                    $sheet->setCellValue('F1', 'Total rifas');
+                    $sheet->setCellValue('G1', $sumPrice);
+                    $sheet->setCellValue('F2', 'Abonado total');
+                    $sheet->setCellValue('G2', $sumPayment);
+                    $sheet->setCellValue('F3', 'Saldo pendiente');
+                    $sheet->setCellValue('G3', ($sumPrice - $sumPayment));
+                    $sheet->setCellValue('A3', 'No. Boletas:  ' . $countTicket);
+                    $sheet->setCellValue('A4', 'Vendedor(a): ' . $user->name." ".$user->lastname);
+                },
+            ];
+        }else{
+            return [];
+        }
+
+        
     }
 }
