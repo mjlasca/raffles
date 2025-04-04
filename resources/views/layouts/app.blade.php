fad10192cd6de8435a8cfb6ab9835e31334b259c
Mario Lasluisa <mjlasca@gmail.com>
2025-04-03 21:43:18 -0500

    secretaria show raffles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -103,7 +103,7 @@
                             </a>
                         </li>
 
-                        @if (auth()->user()->role === 'Administrador')
+                        @if (auth()->user()->role === 'Administrador' || auth()->user()->role === 'Secretaria')
                             <li class="mb-2">
                                 <a href="/rifas" class="inline-flex items-center @if( str_contains( request()->path(),'rifas') ) bg-green-500 @else bg-blue-500 @endif  px-3 py-3 border  text-sm font-lg rounded-md  hover:bg-green-500 hover:text-white focus:outline-none transition ease-in-out duration-150 w-40">
                                     <span class="mr-2 p-1 rounded-md bg-blue-500">
